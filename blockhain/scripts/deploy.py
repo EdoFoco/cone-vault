9a8b332dc7537cfadeac5d5cc716de4b0eae3ec0
BitFire32 <>
2022-05-31 19:04:43 +0200

    fix: deploy script

--- blockhain/scripts/deploy.py
+++ blockhain/scripts/deploy.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
-from brownie import Cubo, CuboDaoV1, Dai, accounts
-
+from brownie import Cubo, CuboDaoV1, Dai, accounts, ConeVaultFactory
 
 def main():
     dai = Dai.deploy({'from': accounts[0]})
     cubo = Cubo.deploy({'from': accounts[0]})
     dao = CuboDaoV1.deploy(cubo, dai,{'from': accounts[0]})
-    print(dao)
-    cubo.setDaoContract(dao, {'from': accounts[0]})
-    return
+    factory = ConeVaultFactory.deploy(accounts[1], dao, cubo, dai, {'from': accounts[0]})
+
+    
+    # print(dao)
+    # cubo.setDaoContract(dao, {'from': accounts[0]})
